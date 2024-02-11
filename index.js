62d67780e077dccdf35d26ebf781ef0e6e2e5f25
Jo4oM4rtins <158056560+Jo4oM4rtins@users.noreply.github.com>
2024-02-11 15:03:25 -0300

    CODIGO PAI

--- index.js
+++ index.js
@@ -1,95 +1,96 @@
 const perguntas = [
-    {
-      pergunta: "Qual é a finalidade do comando 'console.log()' em JavaScript?",
-      respostas: [
-        "Exibir uma mensagem de erro",
-        "Imprimir dados no console",
-        "Criar uma variável"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é a função do operador '===' em comparações em JavaScript?",
-      respostas: [
-        "Comparação de valores sem considerar o tipo",
-        "Atribuição de valores",
-        "Comparação estrita de valores e tipos"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Como se declara uma variável em JavaScript?",
-      respostas: [
-        "let myVar;",
-        "const myVar = 10;",
-        "ambas as opções acima estão corretas"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "O que é uma função em JavaScript?",
-      respostas: [
-        "Um tipo de dado",
-        "Um bloco de código reutilizável",
-        "Uma variável global"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é a diferença entre 'let' e 'const' na declaração de variáveis?",
-      respostas: [
-        "Nenhuma, são sinônimos",
-        "let é usado para valores constantes, const para variáveis",
-        "let permite reatribuição, const cria variáveis imutáveis"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "O que é o DOM em JavaScript?",
-      respostas: [
-        "Um método de criptografia",
-        "Um modelo de objeto para manipular documentos HTML",
-        "Uma linguagem de programação"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Como se realiza uma iteração sobre os elementos de um array em JavaScript?",
-      respostas: [
-        "Usando a estrutura 'if-else'",
-        "Com a declaração 'switch'",
-        "Utilizando loops como 'for' ou 'forEach'"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "O que é o JSON em JavaScript?",
-      respostas: [
-        "Um método de formatação de texto",
-        "Uma linguagem de estilização",
-        "Um formato de dados leve e intercambiável"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Qual é a diferença entre 'null' e 'undefined' em JavaScript?",
-      respostas: [
-        "São iguais, usados de forma intercambiável",
-        "'null' representa a ausência de valor, 'undefined' é atribuído explicitamente",
-        "Ambos representam valores vazios"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Como se adiciona um evento a um elemento HTML usando JavaScript?",
-      respostas: [
-        "Apenas com CSS",
-        "Usando o atributo 'event'",
-        "Através do método 'addEventListener'"
-      ],
-      correta: 2
-    },
-  ];
+  {
+    pergunta: "Quem é Jesus na fé cristã?",
+    respostas: [
+      "Um profeta",
+      "O Filho de Deus",
+      "Um líder político"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Onde Jesus nasceu?",
+    respostas: [
+      "Nazareth",
+      "Jerusalém",
+      "Belém"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual dos seguintes não é um dos doze apóstolos de Jesus?",
+    respostas: [
+      "Pedro",
+      "João",
+      "Judas Iscariotes"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Quantos anos Jesus tinha quando começou seu ministério público?",
+    respostas: [
+      "25",
+      "30",
+      "35"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é o significado do nome 'Jesus'?",
+    respostas: [
+      "Salvador",
+      "Curador",
+      "Profeta"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "O que Jesus fez em seu ministério?",
+    respostas: [
+      "Curou os doentes e ensinou sobre o amor de Deus",
+      "Lutou contra os governantes romanos",
+      "Viajou pelo mundo pregando sobre filosofia"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Como Jesus morreu?",
+    respostas: [
+      "Afogado",
+      "De velhice",
+      "Crucificado"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é a mensagem principal do ensinamento de Jesus?",
+    respostas: [
+      "Amar a Deus sobre todas as coisas",
+      "Amar o próximo como a si mesmo",
+      "A busca pelo poder e riqueza"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "O que aconteceu três dias após a morte de Jesus, de acordo com a Bíblia?",
+    respostas: [
+      "Ele ressuscitou dos mortos",
+      "Ele foi esquecido",
+      "Ele apareceu em um sonho para seus discípulos"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é a última ordem de Jesus antes de ascender aos céus?",
+    respostas: [
+      "Distribuir riquezas",
+      "Pregar o Evangelho a todas as nações",
+      "Organizar um exército"
+    ],
+    correta: 1
+  },
+];
+
   
   const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
